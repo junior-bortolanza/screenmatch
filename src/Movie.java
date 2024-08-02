9e03974973c177e6bd679690f0c97e1f08a87c19
Junior Bortolanza <gbortolanzajr@gmail.com>
2024-08-02 16:07:04 -0300

    modified

--- src/Movie.java
+++ src/Movie.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Movie {
+public class Movie {
+    String name;
+    int releaseYear;
+    boolean planIncluded;
+    double sumAvaliation;
+    int totalAvaliation;
+    int durationMinute;
+
+    void displayDataSheet(){
+        System.out.println("Movie name:" + name);
+        System.out.println("Release Year: "+ releaseYear);
+
+    }
+    void avalia(double note){
+        sumAvaliation += note;
+        totalAvaliation++;
+    }
+    double average(){
+        return sumAvaliation / totalAvaliation;
+    }
+
+
 }
--- src/Program.java
+++ src/Program.java
@@ -1,15 +1,26 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Program {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Movie myMovie = new Movie();
+        myMovie.name = "The Godfather";
+        myMovie.releaseYear = 1970;
+        myMovie.durationMinute = 180;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        myMovie.displayDataSheet();
+        System.out.println(myMovie);
+
+        myMovie.avalia(8);
+        myMovie.avalia(9);
+        myMovie.avalia(7);
+        myMovie.avalia(6);
+
+        System.out.println(myMovie.sumAvaliation);
+        System.out.println(myMovie.totalAvaliation);
+        System.out.println(myMovie.average());
+
+
+
+
     }
 }
